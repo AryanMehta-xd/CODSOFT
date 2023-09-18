3c88488b092f60fe1cd364a511d138b711ae7279
zolan30 <mehtaaryan207@gmail.com>
2023-09-18 23:12:03 +0530

    Random Number Generator Game v01

---
+++ Random Number Game/RandomNumberGame/src/randomnumbergame/RandomNumberGame.java
@@ -0,0 +1,14 @@
+package randomnumbergame;
+
+/**
+ *
+ * @author Aryan Mehta
+ */
+//Main class which will execute the entire application
+public class RandomNumberGame {
+
+    public static void main(String[] args) {
+        new mainApp().setVisible(true);
+    }
+    
+}
