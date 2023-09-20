dc551192cd7f12285f81abe9936a231b767df8d3
zolan30 <mehtaaryan207@gmail.com>
2023-09-20 23:49:07 +0530

    Word Processor v01

---
+++ wordCounter/src/wordcounter/WordCounter.java
@@ -0,0 +1,16 @@
+package wordcounter;
+
+/**
+ *
+ * @author Aryan Mehta
+ */
+public class WordCounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new fileSelecter().setVisible(true);
+    }
+    
+}
